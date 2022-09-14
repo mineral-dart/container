a4ddf47c02b843eb720602af2cdbdc7dc2d05211
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-09-14 17:16:47 +0200

    feat: Change enum visibility

--- lib/src/service.dart
+++ lib/src/service.dart
@@ -1,13 +1,13 @@
 class Service {
-  final http = 'Mineral/Core/Http';
-  final client = 'Mineral/Core/Client';
-  final environment = 'Mineral/Core/Environment';
-  final event = 'Mineral/Core/Event';
-  final command = 'Mineral/Core/Command';
-  final store = 'Mineral/Core/Store';
-  final modules = 'Mineral/Core/Modules';
-  final cli = 'Mineral/Core/Cli';
-  final shards = 'Mineral/Core/Shards';
-  final reporter = 'Mineral/Core/Reporter';
-  final contextMenu = 'Mineral/Core/ContextMenu';
+  static final http = 'Mineral/Core/Http';
+  static final client = 'Mineral/Core/Client';
+  static final environment = 'Mineral/Core/Environment';
+  static final event = 'Mineral/Core/Event';
+  static final command = 'Mineral/Core/Command';
+  static final store = 'Mineral/Core/Store';
+  static final modules = 'Mineral/Core/Modules';
+  static final cli = 'Mineral/Core/Cli';
+  static final shards = 'Mineral/Core/Shards';
+  static final reporter = 'Mineral/Core/Reporter';
+  static final contextMenu = 'Mineral/Core/ContextMenu';
 }
